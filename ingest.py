58b2abaf0b59f721c0540b66493eec04b5c7ded0
Johnsylva <90647997+Johnsylva@users.noreply.github.com>
2026-01-04 11:37:43 -0600

    Add data ingestion script

---
+++ ingest.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+from pinecone import Pinecone
+import re # regular expression - regex
+
+load_dotenv()
+
+
+def split_markdown_by_h2(md_text):
+    """Converts Markdown file into a list of text chunks. Each chunk spans one section of the text, defined by H2s"""
+    # regex - pattern is from H2 to next H2 or EOF
+    pattern = r"(?m)^##.+?(?=^## |\Z)"
+    chunks = re.findall(pattern, md_text, re.DOTALL)
+    return [chunk.strip() for chunk in chunks if chunk.strip()]
+
+# Grab the documentation from the cocktail docs
+with open("cocktails.md", "r", encoding="utf-8") as file:
+    documentation = file.read()
+
+# Pass in the documentation to split it into relevant chunks
+chunks = split_markdown_by_h2(documentation)
+
+print(f"Found {len(chunks)} chunks")
+
+# Wrap each chunk in the record format that Pinecone expects:
+records = []
+for i, chunk in enumerate(chunks):
+    records.append({
+        "id": f"chunk-{i}",
+        "chunk_text": chunk,
+        "manual": "cocktail"
+    }
+    )
+
+#Upload/upsert the chunks into the Pinecone index
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+dense_index = pc.Index("rag-project")
+
+# Pinecone can only handle 96 records at a time, so we upload in batches
+BATCH_SIZE = 96
+for i in range(0, len(records), BATCH_SIZE):
+    batch = records[i:i + BATCH_SIZE]
+    dense_index.upsert_records("flamehamster", batch)
+    print(f"Upserted batch {i // BATCH_SIZE + 1} ({len(batch)} records)")
+
+print(f"Done! Uploaded {len(records)} chunks to Pinecone.")
+
